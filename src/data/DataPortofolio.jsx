3cfd8400b595f89107e6c1a8e8a36381f470765a
Alvin Rachman Septian <alvinrachman16@gmail.com>
2024-11-07 15:20:10 +0700

    create page Detail Portofolio

--- src/data/DataPortofolio.jsx
+++ src/data/DataPortofolio.jsx
@@ -8,26 +8,38 @@ import portofolio6 from '../assets/portfolio6.png'
 export const portofolioList = [
     {
         id: '1',
-        image: portofolio1
+        image: portofolio1,
+        title: 'Website Portofolio #1',
+        skill: ['react', 'html', 'css'],
     },
     {
         id: '2',
-        image: portofolio2
+        image: portofolio2,
+        title: 'Website Portofolio #2',
+        skill: ['react', 'html', 'css'],
     },
     {
         id: '3',
-        image: portofolio3
+        image: portofolio3,
+        title: 'Website Portofolio #3',
+        skill: ['react', 'html', 'css'],
     },
     {
         id: '4',
-        image: portofolio4
+        image: portofolio4,
+        title: 'Website Portofolio #4',
+        skill: ['react', 'html', 'css'],
     },
     {
         id: '5',
-        image: portofolio5
+        image: portofolio5,
+        title: 'Website Portofolio #5',
+        skill: ['react', 'html', 'css'],
     },
     {
         id: '6',
-        image: portofolio6
+        image: portofolio6,
+        title: 'Website Portofolio #6',
+        skill: ['react', 'html', 'css'],
     },
 ]
--- src/pages/DetailPortofolio.jsx
+++ src/pages/DetailPortofolio.jsx
@@ -1,11 +1,35 @@
 import { useParams } from "react-router-dom"
+import '../styles/DetailPortofolio.css'
+import { portofolioList } from '../data/DataPortofolio'
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 function DetailPortofolio() {
 
     const {id} = useParams()
+    const data = portofolioList.find((item)=> item.id == id)
+    console.log(data)
 
     return (
-        <div>DetailPortofolio {id} </div>
+        <>
+            <Navbar />
+            <section id="detail-portofolio">
+                <div className="wrapper">
+                    <h1>{data.title}</h1>
+                    <img src={data.image} />
+                    <p className="skill">
+                        {
+                            data.skill.map((item, index)=>{
+                                return (
+                                    item + ' '
+                                )
+                            })
+                        }
+                    </p>
+                </div>
+            </section>
+            <Footer />
+        </>
     )
 }
 
