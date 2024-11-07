0c0956d9fbf899e1a3b6919bb88bdd76760e3d5f
Alvin Rachman Septian <alvinrachman16@gmail.com>
2024-11-07 16:38:10 +0700

    install react-vertical-timeline for experiance page

--- src/pages/Experiance.jsx
+++ src/pages/Experiance.jsx
@@ -1,6 +1,33 @@
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+import { FaPhp, FaHtml5, FaJs, FaReact, FaBootstrap, FaLaravel, FaNode, FaGithub } from "react-icons/fa";
+import { FaGolang } from "react-icons/fa6";
+import { PiFileCssLight } from "react-icons/pi";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { DiCodeigniter } from "react-icons/di";
+
 function Experiance() {
   return (
-    <div>Experiance</div>
+    <>
+        <div className="wrapper">
+            <h1>Experiance</h1>
+            <VerticalTimeline>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="2010 - 2011"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    icon={<FaReact />}
+                >
+                    <h3 className="vertical-timeline-element-title">Art Director</h3>
+                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                    <p>
+                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                    </p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+        </div>
+    </>
   )
 }
 
