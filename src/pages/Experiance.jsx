b4753d3b4c8834b10916845df9ee509197acae1f
Alvin Rachman Septian <alvinrachman16@gmail.com>
2024-11-08 17:03:40 +0700

    update experiance page

--- src/pages/Experiance.jsx
+++ src/pages/Experiance.jsx
@@ -7,9 +7,14 @@ import { PiFileCssLight } from "react-icons/pi";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { DiCodeigniter } from "react-icons/di";
 
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import ScrollToTop from "../components/ScrollToTop"
+
 function Experiance() {
   return (
     <>
+        <Navbar />
         <div className="wrapper">
             <h1>Experiance</h1>
             <VerticalTimeline>
@@ -25,8 +30,46 @@ function Experiance() {
                     Creative Direction, User Experience, Visual Design, SEO, Online Marketing
                     </p>
                 </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="2010 - 2011"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    icon={<FaReact />}
+                >
+                    <h3 className="vertical-timeline-element-title">Art Director</h3>
+                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                    <p>
+                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="2010 - 2011"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    icon={<FaReact />}
+                >
+                    <h3 className="vertical-timeline-element-title">Art Director</h3>
+                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                    <p>
+                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    date="2010 - 2011"
+                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    icon={<FaReact />}
+                >
+                    <h3 className="vertical-timeline-element-title">Art Director</h3>
+                    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                    <p>
+                    Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+                    </p>
+                </VerticalTimelineElement>
             </VerticalTimeline>
         </div>
+        <Footer />
+        <ScrollToTop />
     </>
   )
 }
